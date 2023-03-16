cb80cd0564a5fe429f24902d93e47fb273c486da
JunoV456 <126973812+JunoV456@users.noreply.github.com>
2023-03-16 14:05:10 -0400

    Add files via upload
    Updated the layout of GuestView and added some text alterations to the controller. Still trying to figure out a proper sorting algorithm.

--- GuestViewController.java
+++ GuestViewController.java
@@ -2,24 +2,27 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 public class GuestViewController {
 
    @FXML
    private VBox LeaderboardVBox;
    private Label[] leaderboard;
-    
+       
    public void initialize() {
       try {
          Database data = new Database();
          System.out.println(data.getTeamsList());
          this.leaderboard = new Label[data.teams.size()];
          
-         //Add sort for point
+         System.out.println(data.teams.get(1).getPoints()); //Don't mind this, was checking something
+         //Add sort for points
          
          for(int i = 0; i < data.teams.size(); i++) {
             leaderboard[i] = new Label();
-            leaderboard[i].setText(data.teams.get(i).getName() + " " + data.teams.get(i).getPoints());
+            leaderboard[i].setFont(new Font("Jokerman", 13));
+            leaderboard[i].setText("Rank #" + (i+1) + " " + data.teams.get(i).getName() + " | Score: " + data.teams.get(i).getPoints() + "\n\n");
             LeaderboardVBox.getChildren().add(leaderboard[i]);
          }
       }
