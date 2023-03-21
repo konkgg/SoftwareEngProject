6ce99cba863bb373ab1525f1c80afbe09d078da7
JunoV456 <126973812+JunoV456@users.noreply.github.com>
2023-03-21 13:49:45 -0400

    Add files via upload
    Sorting is not complete, but I got something cooking
    I also adjusted the size of the Team Display area

--- GuestViewController.java
+++ GuestViewController.java
@@ -16,13 +16,30 @@ public class GuestViewController {
          System.out.println(data.getTeamsList());
          this.leaderboard = new Label[data.teams.size()];
          
-         System.out.println(data.teams.get(1).getPoints()); //Don't mind this, was checking something
-         //Add sort for points
+         /*System.out.println(data.teams.get(1).getPoints()); //Don't mind this, was checking something
+         
+         if (data.teams.get(1).getPoints() < data.teams.get(2).getPoints())
+            System.out.println("Epic");
+         else if (data.teams.get(1).getPoints() > data.teams.get(2).getPoints())
+            System.out.println("Wowzers");
+         
+         TeamObject newt = data.getTeam("team" + 5);
+         TeamObject oldt = data.getTeam("team" + 1);
+         TeamObject temp = new TeamObject("temp", 0);
+         
+         data.replaceTeam("team" + 5, temp);
+         data.replaceTeam("team" + 1, newt);
+         data.replaceTeam("temp", oldt);*/
+         
+         for(int i = 1; i < data.teams.size(); i++) { 
+            int j = i - 1;
+            
+         }
          
          for(int i = 0; i < data.teams.size(); i++) {
             leaderboard[i] = new Label();
             leaderboard[i].setFont(new Font("Jokerman", 13));
-            leaderboard[i].setText("Rank #" + (i+1) + " " + data.teams.get(i).getName() + " | Score: " + data.teams.get(i).getPoints() + "\n\n");
+            leaderboard[i].setText(" Rank #" + (i+1) + " | " + data.teams.get(i).getName() + " | Score: " + data.teams.get(i).getPoints() + "\n\n");
             LeaderboardVBox.getChildren().add(leaderboard[i]);
          }
       }
