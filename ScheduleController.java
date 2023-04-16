07ec39ce72f4a00eed9b71d052cb22b5a150d8e6
Konrad <98121791+ksturdivantwilson@users.noreply.github.com>
2023-04-16 14:34:40 -0400

    Update ScheduleController.java

--- ScheduleController.java
+++ ScheduleController.java
@@ -15,6 +15,13 @@ public class ScheduleController {
       Database data = new Database();
       
       weeks = new VBox[data.getSchedule().getTotalWeeks()];
+      
+      //for loop that for each week adds labels to the vbox for that week
+      
+         //create a label saying the week
+         
+         //create labels for each game that week saying who is playing
+         //show who wins each week
       }
       catch(Exception e) {
          e.printStackTrace();
