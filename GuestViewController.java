abf26063197f3344d176c20ad9b26cde92c5312f
JunoV456 <126973812+JunoV456@users.noreply.github.com>
2023-04-16 13:02:07 -0400

    Add files via upload

--- GuestViewController.java
+++ GuestViewController.java
@@ -1,5 +1,6 @@
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 
@@ -7,30 +8,17 @@ public class GuestViewController {
 
    @FXML
    private VBox LeaderboardVBox;
-   private VBox ScheduleVBox;
+   private HBox ScheduleHBox;
    private Label[] leaderboard;
+   private Label[] schedule;
        
    public void initialize() {
       try {
          Database data = new Database();
-         System.out.println(data.getTeamsList());
          this.leaderboard = new Label[data.getTeamsList().size()];
+         //add relevant schedule related values
          
-         /* System.out.println(data.teams.get(1).getPoints()); //Don't mind this, was checking something
-         
-         if (data.teams.get(1).getPoints() > data.teams.get(2).getPoints())
-            System.out.println("Epic");
-         else if (data.teams.get(1).getPoints() < data.teams.get(2).getPoints())
-            System.out.println("Wowzers");
-         
-         TeamObject newt = data.getTeam("team" + 5);
-         TeamObject oldt = data.getTeam("team" + 1);
-         TeamObject temp = new TeamObject("temp", 0);
-         
-         data.replaceTeam("team" + 5, temp);
-         data.replaceTeam("team" + 1, newt);
-         data.replaceTeam("temp", oldt); */
-         
+         //Sorting Teams by Score
          for(int i = 0; i < data.getTeamsList().size(); i++) {
             for(int j = 0; j < data.getTeamsList().size(); j++) {
                TeamObject newt;
@@ -45,16 +33,20 @@ public class GuestViewController {
                   System.out.println("Swap");
                }
                else
-                  System.out.println("Oh No");  
+                  System.out.println("Stay");  
             }
          }
          
+         //Adding Teams to VBox
          for(int i = 0; i < data.getTeamsList().size(); i++) {
             leaderboard[i] = new Label();
             leaderboard[i].setFont(new Font("Georgia", 12));
             leaderboard[i].setText("   " + data.getTeamsList().get(i).getName() + " | Score: " + data.getTeamsList().get(i).getPoints() + "\n\n");
             LeaderboardVBox.getChildren().add(leaderboard[i]);
          }
+         
+         //Figure out how to deal with schedules
+         
       }
       catch(Exception e) {
          e.printStackTrace();
